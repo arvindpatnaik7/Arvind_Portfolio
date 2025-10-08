c3d10cc3f84d3c723fe3f8583feede9fb87d92eb
arvindpatnaik7 <arvindguggilapu@gmail.com>
2025-10-08 18:29:00 +0530

    Initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://voices-of-java-dev.onrender.com/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/arvindpatnaik7/Arvind_Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
